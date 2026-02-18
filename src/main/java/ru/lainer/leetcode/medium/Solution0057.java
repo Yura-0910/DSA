66761ea6f29b9bed5b2694f07a62b2c968483ba6
Yura Mazurov <mazurovyura09@yandex.ru>
2026-02-18 15:47:36 +0300

    057. Medium. Insert Interval(Вариант № 2)

--- src/main/java/ru/lainer/leetcode/medium/Solution0057.java
+++ src/main/java/ru/lainer/leetcode/medium/Solution0057.java
@@ -1,7 +1,9 @@
 package ru.lainer.leetcode.medium;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.lang.Math;
 
 public class Solution0057 {
 
@@ -119,12 +121,66 @@ public class Solution0057 {
         .toArray(int[][]::new);
   }
 
+  public int[][] insert2(int[][] intervals, int[] newInterval) {
+    int[][] arrSrc = new int[intervals.length + 1][2];
+    int indexSrc = 0; //Индекс для заполнения arrSrc
+    boolean addedToList = false;
+
+    for(int[] currentArr: intervals){
+      arrSrc[indexSrc] = currentArr;
+      indexSrc++;
+    }
+    //Добавляем еще newInterval
+    arrSrc[indexSrc] = newInterval;
+
+    //Сортируем массив по первому элементу каждого одномерного массива
+    Arrays.sort(arrSrc, (el1, el2) -> Integer.compare(el1[0], el2[0]));
+
+    List<int[]> listResult = new ArrayList<>();
+
+    //Инициализируем начальными данными
+    int curLeft  = arrSrc[0][0];
+    int curRight = arrSrc[0][1];
+
+    //Обрабатываем массив arrSrc
+    for(int i = 1; i < arrSrc.length; i++){
+      if(arrSrc[i][0] <= curRight){
+        curRight = Math.max(curRight, arrSrc[i][1]);
+        addedToList = false;
+      }
+      else{
+        int[] pair = new int[2];
+        pair[0] = curLeft;
+        pair[1] = curRight;
+        listResult.add(pair);
+        addedToList = true;
+        curLeft  = arrSrc[i][0];
+        curRight = arrSrc[i][1];
+      }
+    }
+
+    //Добавляем пересекающиеся отрезки
+    if(addedToList == false){
+      int[] pair = new int[2];
+      pair[0] = curLeft;
+      pair[1] = curRight;
+      listResult.add(pair);
+    }
+    //Добавляем последний отрезок
+    if(addedToList == true){
+      listResult.add(arrSrc[arrSrc.length - 1]);
+    }
+
+    return listResult.toArray(new int[listResult.size()][2]);
+  }
+
   public static void main(String[] args) {
     int[][] intervals = {{1, 4}, {9, 12}, {19, 22}};
     int[] newInterval = {7, 13};
 
     var solution = new Solution0057();
-    int[][] result = solution.insert(intervals, newInterval);
+    //int[][] result = solution.insert(intervals, newInterval);
+    int[][] result = solution.insert2(intervals, newInterval);
     for (int[] currentArr : result) {
       System.out.println(Arrays.toString(currentArr));
     }
